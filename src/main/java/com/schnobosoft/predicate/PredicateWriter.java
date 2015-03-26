8ef9052b82a5b303876c9809a2b76bb3d9bb9499
Carsten Schnober <c.schnober@jpberlin.de>
2015-03-26 17:50:05 +0100

    PredicateWriter made abstract.

--- src/main/java/com/schnobosoft/predicate/PredicateWriter.java
+++ src/main/java/com/schnobosoft/predicate/PredicateWriter.java
@@ -1,8 +1,5 @@
 package com.schnobosoft.predicate;
 
-import static org.apache.uima.fit.util.JCasUtil.select;
-import static org.apache.uima.fit.util.JCasUtil.selectCovered;
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,22 +9,17 @@ import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasConsumer_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
-import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 
-import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
 import de.tudarmstadt.ukp.dkpro.core.api.parameter.ComponentParameters;
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma;
 
 /**
  * @author Carsten Schnober
  *
  */
-public class PredicateWriter
+public abstract class PredicateWriter
     extends JCasConsumer_ImplBase
 {
-    private static final String PRED_POS_TAG = "VVFIN";
-    private static final String PARTICLE_POS_TAG = "PTKVZ";
 
     public static final String PARAM_TARGET_LOCATION = ComponentParameters.PARAM_TARGET_LOCATION;
     @ConfigurationParameter(name = PARAM_TARGET_LOCATION, mandatory = false)
@@ -55,42 +47,6 @@ public class PredicateWriter
     };
 
     @Override
-    public void process(JCas jcas)
-        throws AnalysisEngineProcessException
-    {
-
-        String verb = null;
-        String particle = "";
-
-        for (POS pos : select(jcas, POS.class)) {
-            getLogger().debug(String.format("%s\t%s", pos.getCoveredText(), pos.getPosValue()));
-
-            if (pos.getPosValue().equals(PRED_POS_TAG)) {
-                for (Lemma lemma : selectCovered(Lemma.class, pos)) {
-                    assert verb == null;
-                    verb = lemma.getValue();
-                }
-            }
-            else if (pos.getPosValue().equals(PARTICLE_POS_TAG)) {
-                for (Lemma lemma : selectCovered(Lemma.class, pos)) {
-                    assert particle.isEmpty();
-                    particle = lemma.getValue();
-                }
-            }
-        }
-
-        try {
-            writer.write(String.format("%s\t%s%s%n", jcas.getDocumentText(), particle, verb));
-            if (verb == null) {
-                getLogger().warn("No predicate (finite verb) found!");
-            }
-        }
-        catch (IOException e) {
-            throw new AnalysisEngineProcessException(e);
-        }
-    }
-
-    @Override
     public void collectionProcessComplete()
         throws AnalysisEngineProcessException
     {
@@ -101,5 +57,11 @@ public class PredicateWriter
         catch (IOException e) {
             throw new AnalysisEngineProcessException(e);
         }
+    }
+
+
+    public BufferedWriter getWriter()
+    {
+        return writer;
     }
 }
